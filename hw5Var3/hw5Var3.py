b1761ad09275d0e0cb4b1dcd214751c1bfc9bd52
Masha01 <Masha.ev2012@yandex.ru>
2016-12-05 20:10:46 +0300

    Add files via upload

--- hw5Var3/hw5Var3.py
+++ hw5Var3/hw5Var3.py
@@ -4,13 +4,14 @@ f = open( "Капибара.txt", "r", encoding = "utf-8")
 for line in f:
     arr = line.split()
     for i in arr:
-        if len(i) == 3 and i[len(i)-1] != ',' :
-           first += 1
-        if len(i) == 4 and i[len(i)-1] == ',':
-        #Знаю, что для универсальности нужно добавить такие условия для других знаков препинания(и в слове длины 1 тоже),с таким условием программа работает только для моего текста.Но из задания не совсем поняла, должна ли быть программа универсальной.
+        if len(i) == 3 and i[len(i)-1] != ',' and i[len(i)-1] != '.' and i[len(i)-1] != ':' and i[len(i)-1] != ';' and i[len(i)-1] != '!' and i[len(i)-1] != '?':
+            first += 1
+        if len(i) == 4 and i[len(i)-1] == ',' for i[len(i)-1] == '.' or i[len(i)-1] == ':' or i[len(i)-1] == ';' or i[len(i)-1] == '!' or i[len(i)-1] == '?':
             first += 1
         if len(i) == 1 and i != '―':
             second += 1
+        if len(i) == 2 and i[len(i)-1] == ',' or i[len(i)-1] == '.' or i[len(i)-1] == ':' or i[len(i)-1] == ';' or i[len(i)-1] == '!' or i[len(i)-1] == '?':
+            second +=1
 if second == 0:
     print( 'Слов длины один нет')
 else:
