e8dfb1a4f461305333d79c5b38dab542445c3883
Masha01 <Masha.ev2012@yandex.ru>
2017-04-10 17:59:22 +0400

    Add files via upload

---
+++ hw12Var3/hw12Var3_исправленный.py
@@ -0,0 +1,20 @@
+import re
+def open_text():
+    with open('Гоголь.txt', 'r', encoding = 'utf - 8') as f:
+        text = f.read()
+        text = re.sub(r'\n',' ', text)
+        arr = re.split('\.|\?|\! ', text)
+        for i, s in enumerate(arr):
+            arr[i] = re.sub(r'[:;,.?!— -]',' ', arr[i])
+        return arr 
+
+def words_5():
+    arr = open_text()
+    for i in range(len(arr)):
+        arr1 = arr[i].split()
+        template = '{}_{}'
+        length =[template.format(arr1[i],len(arr1[i])) for i,w in enumerate(arr1)]
+        for i in range(len(length)):
+            print(length[i])
+    return 
+words_5()
