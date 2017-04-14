869cf48737254b2aace9d64856914d1d6f20c95c
Masha01 <Masha.ev2012@yandex.ru>
2017-04-14 21:05:09 +0400

    Add files via upload

---
+++ hw13Var3.py
@@ -0,0 +1,20 @@
+import re
+import os
+def folder():
+    arr = [f for f in os.listdir('.')if re.search(r'[а-яА-Я]+',f)if os.path.isdir(f)]
+    print(len(arr))
+    return arr
+folder()
+
+def print_result():
+    result = []
+    for f in os.listdir('.'):
+        if os.path.isfile(f):
+            f = f[:f.rfind('.')]
+            if f not in result:
+                result.append(f)
+        else:
+            if f not in result:
+                result.append(f)
+    return ' '.join([str(i) for i in result])
+print(print_result())
